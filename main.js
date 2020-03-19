65b3e823bb9cc89c79fe5b6ba93894e947ac045e
Conner Foster <connerf99@gmail.com>
2020-03-19 18:31:21 -0400

    possibly finished

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 const grid = document.getElementById('grid');
-for (i=0; i<256; i++){
+const resetButton = document.querySelector("#reset");
+const resButton = document.querySelector("#resolution");
+let size = 32;
+createSquare(size);
+
+function createSquare(size) {
+for (i=0; i<(size*size); i++){
     let square = document.createElement('div');
     square.classList.add('squares');
     grid.appendChild(square);
+
 }
+const divs = document.querySelectorAll(".squares")
+divs.forEach(square => {
+    square.addEventListener('mouseenter', ()=> {
+        square.style.backgroundColor = "black";
+    })
+});
+
+
+}
+
+
+
+
+resetButton.addEventListener("click", reset);
+function reset(){
+    location.reload();
+}
+
+function changeRes() {
+
+}
+
+
+
+
+
+
+
+
